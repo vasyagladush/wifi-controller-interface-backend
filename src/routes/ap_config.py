4629b0b28ffb17327dfd62a85002e24a8eda3b9c
noname135E <michal.jankowski800@gmail.com>
2024-06-09 17:04:23 +0200

    Tried to setup model_configs
    Simplified web handling to be more in-line with other code.

--- src/routes/ap_config.py
+++ src/routes/ap_config.py
@@ -2,17 +2,15 @@ from fastapi import APIRouter, HTTPException
 
 from config import DBSessionDep
 from models import AP
-from services.AP import get_AP
+from schemas.AP import APSchema
+from services.AP import get_AP, get_AP_by_name
 
 router = APIRouter()
 
 # TODO: Implement post logic
 
 
-@router.get(
-    "/{id}",
-    status_code=200,
-)
+@router.get("/{id}", status_code=200, response_model=APSchema)
 async def get_config_id(id, db_session: DBSessionDep):
     try:
         id = int(id)
@@ -24,14 +22,12 @@ async def get_config_id(id, db_session: DBSessionDep):
     ap = await get_AP(db_session, id)
     if ap is None:
         raise HTTPException(status_code=400, detail="Invalid Device ID")
-    return await get_config(ap, db_session)
+    return ap
 
 
-# TODO: Implement get by name
-
-
-async def get_config(ap: AP, db_session: DBSessionDep):
-
-    # TODO: Figure out how to get data from DB
-    # TODO: Add handling of networks
-    return f"OK, id: {ap.device_id}"
+# TODO: Implement router for config?name={name}
+async def get_config_name(name, db_session: DBSessionDep):
+    ap = await get_AP_by_name(db_session, name)
+    if ap is None:
+        raise HTTPException(status_code=400, detail="Invalid Device Name")
+    return ap
--- src/schemas/AP.py
+++ src/schemas/AP.py
@@ -1,8 +1,14 @@
+from pydantic import ConfigDict
+
 from . import BaseSchema
 
 
 class NetworkNameSchema(BaseSchema):
     name: str
+
+    model_config = ConfigDict(
+        from_attributes=True, json_schema_extra={"example": {"name": "Guests"}}
+    )
 
 
 class APSchema(BaseSchema):
@@ -12,3 +18,18 @@ class APSchema(BaseSchema):
     networks: list[
         NetworkNameSchema
     ]  # TODO: Verify that this how this should be done
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "example": {
+                "device_id": 5,
+                "name": "Floor 1 AP",
+                "ip": "192.168.1.6",
+                "networks": [
+                    {"name": "Guests"},
+                    {"name": "Employees"},
+                ],
+            }
+        },
+    )

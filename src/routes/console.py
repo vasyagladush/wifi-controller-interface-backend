f1875b6e566357afa2722bdd3c098b90d1d1c25c
jankalucki <jan.kalucki@outlook.com>
2024-06-13 10:51:35 +0200

    passing commands through named pipes implemented

---
+++ src/routes/console.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Body, HTTPException
+
+from config import app_config
+from schemas.cmd import CmdSchema
+from services.auth import AuthJWTTokenValidatorDep
+from services.console import send_command
+
+router = APIRouter(
+    dependencies=[AuthJWTTokenValidatorDep], responses={401: {}}
+)
+
+
+@router.post("/console", status_code=200)
+async def cmd_input(command: CmdSchema = Body(...)):
+    output = await send_command(
+        app_config.CMD_PIPE_PATH,
+        app_config.OUTPUT_PIPE_PATH,
+        command.cmd,
+        command.args,
+    )
+    return {"output": output}
--- src/services/console.py
+++ src/services/console.py
@@ -1,18 +1,13 @@
-import imp
-from subprocess import PIPE, Popen
+import aiofiles
 
 
-def connect_to_wlc_cli(wlc_cli_path: str) -> Popen:
-    try:
-        return Popen(
-            f"python3 {wlc_cli_path}", stdin=PIPE, stdout=PIPE, stderr=PIPE
-        )
-    except OSError:
-        raise SystemExit
-
-
-def send_command(wlc_cli_process: Popen, cmd: str, args: list[str]) -> str:
+async def send_command(
+    cmd_pipe: str, output_pipe: str, cmd: str, args: list[str]
+) -> str:
     for arg in args:
         cmd += f" {arg}"
-    with open(wlc_cli_process.stdin, "w") as pipe:
-        pipe.writeline(cmd)
+    with aiofiles.open(cmd_pipe, "w") as pipe_in:
+        await pipe_in.writeline(cmd)
+    with aiofiles.open(output_pipe, "r") as pipe_out:
+        output: str = await pipe_out.readline()
+    return output

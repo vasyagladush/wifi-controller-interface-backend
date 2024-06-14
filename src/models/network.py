85dd10fd89be2036a712625e87b9390ec3fc64db
noname135E <michal.jankowski800@gmail.com>
2024-06-14 22:08:31 +0200

    AP PUT endpoint should now work
    Added network getter.
    
    Changed 'joined' to 'selectin' to match other db-related files.
    
    PUT endpoint for config change of AP implemented - not sure if it fully works, but hasn't caused me issues yet.

--- src/models/network.py
+++ src/models/network.py
@@ -14,7 +14,7 @@ class Network(Base):
         "AccessPoint",
         secondary="ap_network_connectors",
         back_populates="networks",
-        lazy="joined",
+        lazy="selectin",
     )
 
 
--- src/routes/access_points.py
+++ src/routes/access_points.py
@@ -3,6 +3,7 @@ from typing import Annotated, Optional, Sequence
 from fastapi import APIRouter, Depends, HTTPException, Query
 
 import services.access_point as AccessPointService
+import services.network as NetworkService
 from config import DBSessionDep
 from routes import PaginationParamsDep
 from schemas.access_point import APSchema, GetAPsSchema, PutAPSchema
@@ -14,7 +15,6 @@ router = APIRouter(
 )
 
 
-# TODO: Add handling of networks changing
 @router.put("/{id}", status_code=204)
 async def change_ap_config(
     id, config: PutAPSchema, db_session: DBSessionDep
@@ -26,9 +26,17 @@ async def change_ap_config(
     if ap is None:
         raise HTTPException(status_code=400, detail="Invalid ID")
     update_data = dict(config)
-    print(update_data)
+    if "networks" in update_data.keys():
+        nets = update_data["networks"]
+        ap.networks = []
+        for net in nets:
+            net_obj = await NetworkService.get_network(db_session, net.id)
+            if net_obj is None:
+                raise HTTPException(status_code=400, detail="Invalid network")
+            ap.networks.append(net_obj)
+        update_data.pop("networks")
     for entry in update_data.items():
-        if entry[1] != None and entry[0] != "networks":
+        if entry[1] != None:
             setattr(ap, entry[0], entry[1])
     await db_session.commit()
     return
